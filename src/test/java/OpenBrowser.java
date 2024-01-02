6f9e62e5ec048aef688008372ddb6ec11356c2e5
M_Laithy <mahmoudelaithy@gmail.com>
2024-01-02 09:40:31 +0200

    First Test Case

--- src/test/java/OpenBrowser.java
+++ src/test/java/OpenBrowser.java
@@ -2,40 +2,58 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.security.Key;
 
-public class OpenBrowser {
-    public static void main(String[] args) throws InterruptedException {
+public class OpenBrowser
+{
 
-        //1- Bridge between test scripts and browser
+
+    WebDriver driver = null;
+    @BeforeTest
+    public void StartBrowser() throws InterruptedException {
+        //Bridge between test scripts and browser
         String chromePath = System.getProperty("user.dir") + "\\src\\main\\resources\\Browsers\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver",chromePath);
-
-        //2- New Object of WebDriver
-        WebDriver driver = new ChromeDriver();
-        
-        //3- Navigate to Google website
+        //New Object of WebDriver
+        driver = new ChromeDriver();
+        //Navigate to Google website
         driver.navigate().to("https://the-internet.herokuapp.com/login");
         driver.manage().window().maximize(); //Maximize
         Thread.sleep(1000); //Wait for 3 secs
+    }
 
-        //3.2- Actions
+    @Test
+    public void Test01_ValidData () {
         //Entering Credentials
         driver.findElement(By.name("username")).clear();
         driver.findElement(By.name("username")).sendKeys("tomsmith");
         driver.findElement(By.name("password")).clear();
         driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
-
-        Thread.sleep(1000); //Wait for 3 secs
-
         //Logging in
         driver.findElement(By.name("password")).submit();
-
-        Thread.sleep(1000); //Wait for 3 secs
-
-        //4- Close Driver
-        driver.quit();
-
     }
+
+    @Test
+    public void Test02_InvalidData() {
+        //Entering Credentials
+        driver.findElement(By.name("username")).clear();
+        driver.findElement(By.name("username")).sendKeys("TomBs");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPasswor!");
+        //Logging in
+        driver.findElement(By.name("password")).submit();
+    }
+
+    @AfterTest
+    public void CloseBrowser() throws InterruptedException {
+        //Close Driver
+        Thread.sleep(1000); //Wait for 3 secs
+        driver.quit();
+    }
+
+
 }
