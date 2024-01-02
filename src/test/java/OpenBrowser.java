230d8d610ee8f628076990ea9c10cb044ff6b709
M_Laithy <mahmoudelaithy@gmail.com>
2024-01-02 07:02:31 +0200

    Clean Code For TestNG

--- src/test/java/OpenBrowser.java
+++ src/test/java/OpenBrowser.java
@@ -14,8 +14,7 @@ public class OpenBrowser {
 
         //2- New Object of WebDriver
         WebDriver driver = new ChromeDriver();
-
-
+        
         //3- Navigate to Google website
         driver.navigate().to("https://the-internet.herokuapp.com/login");
         driver.manage().window().maximize(); //Maximize
@@ -31,26 +30,9 @@ public class OpenBrowser {
         Thread.sleep(1000); //Wait for 3 secs
 
         //Logging in
-//        driver.findElement(By.className("radius")).click();                               //Using class name , Delete space and everything after it
-//        driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();            //Using css selector , ("ParentTag > Tag[attribute=\"value\"]")
-//        driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();               //Using xpath , ("//ParentTag//Tag[@attribute=\"value\"]")
-//        driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
         driver.findElement(By.name("password")).submit();
 
         Thread.sleep(1000); //Wait for 3 secs
-
-        //Get Success Msg
-        Thread.sleep(1000); //Wait for 3 secs
-        String msg01 = driver.findElement(By.id("flash")).getText();
-        String msg02 = driver.findElement(By.id("flash")).getAttribute("class");
-        System.out.println(msg01);
-        System.out.println(msg02);
-
-        //Get Msg Color
-        String bgColor = driver.findElement(By.id("flash")).getCssValue("background-color");
-        String color = driver.findElement(By.id("flash")).getCssValue("color");
-        System.out.println(bgColor);
-        System.out.println(color);
 
         //4- Close Driver
         driver.quit();
